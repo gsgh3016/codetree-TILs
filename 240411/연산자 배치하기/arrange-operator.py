0df1a6781089245671c4ee9016dd2da2f8a8b9dc
Gamchan Kang <104420634+gsgh3016@users.noreply.github.com>
2024-04-11 19:30:09 +0900

    Title: 연산자 배치하기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240411/연산자 배치하기/arrange-operator.py
@@ -0,0 +1,52 @@
+import sys
+
+
+ADD = 0
+SUB = 1
+MUL = 2
+
+def cal(a, b, op):
+    if op == ADD:
+        return a + b
+    elif op == SUB:
+        return a - b
+    elif op == MUL:
+        return a * b
+
+input = sys.stdin.readline
+n = int(input())
+nums = list(map(int, input().split()))
+ops = list(map(int, input().split()))
+dp1 = [[0, 0, 0, 0] for _ in range(n)]
+dp2 = [[0, 0, 0, 0] for _ in range(n)]
+dp1[0] = [nums[0], *ops]
+dp2[0] = [nums[0], *ops]
+
+for i in range(1, n):
+    cur_min = 0x4f4f4f4f
+    min_op_idx = -1
+    cur_max = -0x4f4f4f4f
+    max_op_idx = -1
+
+    for j in range(1, 4):
+        if dp1[i - 1][j] > 0 and cal(dp1[i - 1][0], nums[j], j - 1) < cur_min:
+            cur_min = cal(dp1[i - 1][0], nums[i], j - 1)
+            min_op_idx = j
+        if dp2[i - 1][j] > 0 and cal(dp2[i - 1][0], nums[j], j - 1) > cur_max:
+            cur_max = cal(dp2[i - 1][0], nums[i], j - 1)
+            max_op_idx = j
+    
+    dp1[i][0] = cur_min
+    dp2[i][0] = cur_max
+    for j in range(1, 4):
+        if j == min_op_idx:
+            dp1[i][j] = dp1[i - 1][j] - 1
+        else:
+            dp1[i][j] = dp1[i - 1][j]
+        
+        if j == max_op_idx:
+            dp2[i][j] = dp2[i - 1][j] - 1
+        else:
+            dp2[i][j] = dp2[i - 1][j]
+
+print(dp1[n - 1][0], dp2[n - 1][0])
